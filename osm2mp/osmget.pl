5f114139a54b794840267a72ed9ec8b95870daa5
xliosha <xliosha@006ae798-83f3-11dd-8a74-1916a93a1be7>
2008-09-17 17:01:34 +0000

    import

---
+++ osm2mp/osmget.pl
@@ -0,0 +1,51 @@
+
+# use strict;
+require LWP::UserAgent;
+
+
+
+my $bbox = $ARGV[0];
+
+my $api  = "http://api.openstreetmap.org/api/0.5/map?bbox=";
+my $step = 0.5;
+
+
+my @tiles;
+my ($minlon, $minlat, $maxlon, $maxlat) = split ",", $bbox;
+
+for (my $lon=$minlon; $lon<$maxlon; $lon+=$step) {
+    for (my $lat=$minlat; $lat<$maxlat; $lat+=$step) {
+        push @tiles, join (",", ($lon, $lat, (sort($maxlon,$lon+$step))[0], (sort($maxlat,$lat+$step))[0]));
+    }
+}
+
+while (scalar @tiles) {
+
+    my $tile = shift @tiles;
+    print STDERR "Getting bbox=$tile...   ";
+
+    my $ua = LWP::UserAgent->new;
+    my $req = HTTP::Request->new(GET => "$api$tile");
+    my $res = $ua->request($req);
+
+    if ($res->is_success) {
+        print STDERR "Ok\n";
+        print $res->content;
+    } else {
+#        print STDERR $res->status_line . "  --| ";
+        print STDERR $res->code . "  --| ";
+        if ($res->code==400 || $res->code==500) {
+            print STDERR "tile will be splitted\n";
+            my ($lon0, $lat0, $lon1, $lat1) = split ",", $tile;
+            push @tiles, join (",", ($lon0, $lat0, ($lon0+$lon1)/2, ($lat0+$lat1)/2));
+            push @tiles, join (",", ($lon0, ($lat0+$lat1)/2, ($lon0+$lon1)/2, $lat1));
+            push @tiles, join (",", (($lon0+$lon1)/2, $lat0, $lon1, ($lat0+$lat1)/2));
+            push @tiles, join (",", (($lon0+$lon1)/2, ($lat0+$lat1)/2, $lon1, $lat1));
+        } else {
+            print STDERR "shall try again\n";
+            push @tiles, $tile;
+        }
+    }
+
+}
+
