cb92468632bdc35f61ca2ee611642b248921e2dd
xliosha <xliosha@006ae798-83f3-11dd-8a74-1916a93a1be7>
2009-08-05 18:02:30 +0000

    osmsort.pl - bbox calculation

--- trunk/osm2mp/osmsort.pl
+++ trunk/osm2mp/osmsort.pl
@@ -8,13 +8,31 @@ my %rel;
 
 my $obj;
 
+my ($minlat, $minlon, $maxlat, $maxlon) = (90,180,-90,-180);
+
 while (<>) {
 
 #    if ( /<osm/ )                       { $osm = "";     $obj = \$osm;}
     if ( /<node.*\bid=['"]([^'"]+)['"]/ )        { $nod{$1} = ""; $obj = \$nod{$1}; }
     if ( /<way.*\bid=['"]([^'"]+)['"]/ )         { $way{$1} = ""; $obj = \$way{$1}; }
     if ( /<relation.*\bid=['"]([^'"]+)['"]/ )    { $rel{$1} = ""; $obj = \$rel{$1}; }
-    if ( /<bound/ )                     { next; }
+
+    if ( /<bounds?/ )   {
+        my ($minlt, $minln, $maxlt, $maxln);
+        if ( /\<bounds/ ) {
+            ($minlt, $minln, $maxlt, $maxln) 
+                = ( /minlat=["'](\-?\d+\.?\d*)["'] minlon=["'](\-?\d+\.?\d*)["'] maxlat=["'](\-?\d+\.?\d*)["'] maxlon=["'](\-?\d+\.?\d*)["']/ );
+        } 
+        else {
+            ($minlt, $minln, $maxlt, $maxln) 
+                = ( /box=["'](\-?\d+\.?\d*),(\-?\d+\.?\d*),(\-?\d+\.?\d*),(\-?\d+\.?\d*)["']/ );
+        }
+        $minlat = $minlt    if $minlt < $minlat;
+        $minlon = $minln    if $minln < $minlon;
+        $maxlat = $maxlt    if $maxlt > $maxlat;
+        $maxlon = $maxln    if $maxln > $maxlon;
+    }
+
     if ( /<\/?osm/ )                    { next; }
     if ( /<\?xml/ )                     { next; }
 
@@ -24,6 +42,8 @@ while (<>) {
 print "<?xml version='1.0' standalone='yes'?>\n";
 #print $osm;
 print "<osm version=\"0.6\" generator=\"osmsort.pl\">\n";
+print "  <bound box='$minlat,$minlon,$maxlat,$maxlon' origin='http://www.openstreetmap.org/api/0.6'/>\n"
+    if $minlat < $maxlat;
 print @nod{sort {$a<=>$b} keys %nod};
 print @way{sort {$a<=>$b} keys %way};
 print @rel{sort {$a<=>$b} keys %rel};
