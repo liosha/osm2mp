81101e5d817466d6411e7af3fa8e1093c3e5aaae
xliosha <xliosha@006ae798-83f3-11dd-8a74-1916a93a1be7>
2009-11-10 09:57:34 +0000

    osmget: multiple bboxes

--- osm2mp/osmget.pl
+++ osm2mp/osmget.pl
@@ -2,60 +2,65 @@
 
 
 # use strict;
-require LWP::UserAgent;
-use IO::Uncompress::Gunzip qw(gunzip $GunzipError) ;
-
-
-binmode STDOUT;
-
-my $bbox = $ARGV[0];
+use LWP::UserAgent;
+use IO::Uncompress::Gunzip qw{ gunzip $GunzipError };
+use List::Util qw{ min };
 
 my $api  = "http://api.openstreetmap.org/api/0.6/map?bbox=";
 my $step = 0.5;
 #my $api  = "http://www.informationfreeway.org/api/0.5/map?bbox=";
 #my $step = 2;
 
+binmode STDOUT;
 
-my @tiles;
-my ($minlon, $minlat, $maxlon, $maxlat) = split ",", $bbox;
+for my $bbox ( @ARGV ) {;
 
-for (my $lon=$minlon; $lon<$maxlon; $lon+=$step) {
-    for (my $lat=$minlat; $lat<$maxlat; $lat+=$step) {
-        push @tiles, join (",", ($lon, $lat, (sort($maxlon,$lon+$step))[0], (sort($maxlat,$lat+$step))[0]));
-    }
-}
+    print "\nProcessing $bbox\n";
 
-while (scalar @tiles) {
+    my ($minlon, $minlat, $maxlon, $maxlat) = split q{,}, $bbox;
 
-    my $tile = shift @tiles;
-    print STDERR "Getting bbox=$tile...   ";
-
-    my $ua = LWP::UserAgent->new;
-    $ua->default_header('Accept-Encoding' => 'gzip');
-    
-    my $req = HTTP::Request->new(GET => "$api$tile");
-    my $res = $ua->request($req);
-
-    if ($res->is_success) {
-        print STDERR "Ok\n";
-        my $data;
-        gunzip \($res->content) => \$data;
-        print $data;
-    } else {
-#        print STDERR $res->status_line . "  --| ";
-        print STDERR $res->code . "  --| ";
-        if ($res->code==400 || $res->code==500 || $res->code==501) {
-            print STDERR "tile will be splitted\n";
-            my ($lon0, $lat0, $lon1, $lat1) = split ",", $tile;
-            push @tiles, join (",", ($lon0, $lat0, ($lon0+$lon1)/2, ($lat0+$lat1)/2));
-            push @tiles, join (",", ($lon0, ($lat0+$lat1)/2, ($lon0+$lon1)/2, $lat1));
-            push @tiles, join (",", (($lon0+$lon1)/2, $lat0, $lon1, ($lat0+$lat1)/2));
-            push @tiles, join (",", (($lon0+$lon1)/2, ($lat0+$lat1)/2, $lon1, $lat1));
-        } else {
-            print STDERR "shall try again\n";
-            push @tiles, $tile;
+    my @tiles;
+    for (my $lon=$minlon; $lon<$maxlon; $lon+=$step) {
+        for (my $lat=$minlat; $lat<$maxlat; $lat+=$step) {
+            push @tiles, [ $lon, $lat, min( $maxlon, $lon+$step ), min( $maxlat, $lat+$step ) ];
         }
     }
 
-}
+    while (scalar @tiles) {
 
+        my $tile = shift @tiles;
+        my $tilebbox = join q{,}, @$tile;
+        print STDERR "Getting bbox=$tilebbox...   ";
+
+        my $ua = LWP::UserAgent->new;
+        $ua->default_header('Accept-Encoding' => 'gzip');
+
+        my $req = HTTP::Request->new(GET => "$api$tilebbox");
+        my $res = $ua->request($req);
+
+        if ( $res->is_success ) {
+            print STDERR "Ok\n";
+            my $data;
+            gunzip \($res->content) => \$data;
+            print $data;
+        }
+        else {
+            print STDERR $res->code . "  --| ";
+
+            if ( $res->code ~~ [ 400, 500, 501 ] ) {
+                print STDERR "tile will be splitted\n";
+                my ($lon0, $lat0, $lon1, $lat1) = @$tile;
+                push @tiles, 
+                    [ $lon0, $lat0, ($lon0+$lon1)/2, ($lat0+$lat1)/2 ],
+                    [ $lon0, ($lat0+$lat1)/2, ($lon0+$lon1)/2, $lat1 ],
+                    [ ($lon0+$lon1)/2, $lat0, $lon1, ($lat0+$lat1)/2 ],
+                    [ ($lon0+$lon1)/2, ($lat0+$lat1)/2, $lon1, $lat1 ];
+            }
+            else {
+                print STDERR "shall try again\n";
+                push @tiles, $tile;
+            }
+        }
+    }
+}
+    
