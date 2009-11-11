fe2da105f152958897aec4dbff2f2797b9afcc85
xliosha <xliosha@006ae798-83f3-11dd-8a74-1916a93a1be7>
2009-11-11 05:15:05 +0000

    osmget: fix

--- osm2mp/osmget.pl
+++ osm2mp/osmget.pl
@@ -13,54 +13,60 @@ my $step = 0.5;
 
 binmode STDOUT;
 
+my @tiles;
+
 for my $bbox ( @ARGV ) {;
 
-    print STDERR "\nProcessing $bbox\n";
+    print STDERR "Processing $bbox";
 
     my ($minlon, $minlat, $maxlon, $maxlat) = split q{,}, $bbox;
 
-    my @tiles;
+    my $count = 0;
     for (my $lon=$minlon; $lon<$maxlon; $lon+=$step) {
         for (my $lat=$minlat; $lat<$maxlat; $lat+=$step) {
             push @tiles, [ $lon, $lat, min( $maxlon, $lon+$step ), min( $maxlat, $lat+$step ) ];
+            $count ++;
         }
     }
+    print STDERR " -> $count tiles\n";
+}
 
-    while (scalar @tiles) {
 
-        my $tile = shift @tiles;
-        my $tilebbox = join q{,}, @$tile;
-        print STDERR "Getting bbox=$tilebbox...   ";
+while (scalar @tiles) {
 
-        my $ua = LWP::UserAgent->new;
-        $ua->default_header('Accept-Encoding' => 'gzip');
+    my $tile = shift @tiles;
+    my $tilebbox = join q{,}, @$tile;
+    print STDERR "Getting bbox=$tilebbox...   ";
 
-        my $req = HTTP::Request->new(GET => "$api$tilebbox");
-        my $res = $ua->request($req);
+    my $ua = LWP::UserAgent->new;
+    $ua->default_header('Accept-Encoding' => 'gzip');
 
-        if ( $res->is_success ) {
-            print STDERR "Ok\n";
-            my $data;
-            gunzip \($res->content) => \$data;
-            print $data;
+    my $req = HTTP::Request->new(GET => "$api$tilebbox");
+    my $res = $ua->request($req);
+
+    if ( $res->is_success ) {
+        print STDERR "Ok\n";
+        my $data;
+        gunzip \($res->content) => \$data;
+        print $data;
+    }
+    else {
+        print STDERR $res->code . "  --| ";
+
+        if ( $res->code ~~ [ 400, 500, 501 ] ) {
+            print STDERR "tile will be splitted\n";
+            my ($lon0, $lat0, $lon1, $lat1) = @$tile;
+            push @tiles, 
+                [ $lon0, $lat0, ($lon0+$lon1)/2, ($lat0+$lat1)/2 ],
+                [ $lon0, ($lat0+$lat1)/2, ($lon0+$lon1)/2, $lat1 ],
+                [ ($lon0+$lon1)/2, $lat0, $lon1, ($lat0+$lat1)/2 ],
+                [ ($lon0+$lon1)/2, ($lat0+$lat1)/2, $lon1, $lat1 ];
         }
         else {
-            print STDERR $res->code . "  --| ";
-
-            if ( $res->code ~~ [ 400, 500, 501 ] ) {
-                print STDERR "tile will be splitted\n";
-                my ($lon0, $lat0, $lon1, $lat1) = @$tile;
-                push @tiles, 
-                    [ $lon0, $lat0, ($lon0+$lon1)/2, ($lat0+$lat1)/2 ],
-                    [ $lon0, ($lat0+$lat1)/2, ($lon0+$lon1)/2, $lat1 ],
-                    [ ($lon0+$lon1)/2, $lat0, $lon1, ($lat0+$lat1)/2 ],
-                    [ ($lon0+$lon1)/2, ($lat0+$lat1)/2, $lon1, $lat1 ];
-            }
-            else {
-                print STDERR "shall try again\n";
-                push @tiles, $tile;
-            }
+            print STDERR "shall try again\n";
+            push @tiles, $tile;
         }
     }
 }
+
     
