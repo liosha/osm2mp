f1282ea005c0196bbc396d0dc2388cac6accc5c4
xliosha@gmail.com <xliosha@gmail.com@006ae798-83f3-11dd-8a74-1916a93a1be7>
2010-05-02 08:33:27 +0000

    mp postprocessor to fix kladr-bot region names

---
+++ osm2mp/mp-postprocess.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $file = shift @ARGV;
+exit unless $file;
+
+rename $file, "$file.old";
+
+open my $in,  '<', "$file.old";
+open my $out, '>', $file;
+
+while ( my $line = readline $in ) {
+    if ( $line =~ /^(DefaultRegionCountry|RegionName)/i ) {
+        $line =~ s/область/обл./;
+        $line =~ s/район/р-н/;
+        $line =~ s/Автономный округ - Югра автономный округ/АО (Югра)/;
+        $line =~ s/Кабардино-Балкарская республика/Кабардино-Балкария/;
+        $line =~ s/Карачаево-Черкесская республика/Карачаево-Черкесия/;
+        $line =~ s/автономный округ/АО/;
+        $line =~ s/автономная обл./АО/;
+        $line =~ s/ город//;
+        $line =~ s/(ия|[^я]) республика/$1/;
+        $line =~ s/ - Алания//;
+    }
+    print $out $line;
+}
+
+close $in;
+close $out;
+
+unlink "$file.old";
