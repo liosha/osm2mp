fba7280fdc66630a9d5893059ca6a03e606ee08b
xliosha@gmail.com <xliosha@gmail.com@006ae798-83f3-11dd-8a74-1916a93a1be7>
2012-12-04 11:32:59 +0000

    * fixed arabic

--- trunk/lib/LangTransform/Subst.pm
+++ trunk/lib/LangTransform/Subst.pm
@@ -35,13 +35,22 @@ our %DATA = (
         from  => 'ar',
         to    => 'ru',
         table => {
-            ( map {( chr(hex $_) => 'а')}  qw/ 0623 FE83 FE84  0622 FE81 FE82  0649 FEEF FEF0 0627 / ), # أ آ ى
             ( map {( chr(hex $_) => '')}   qw/ 0621 / ), # ﺀ 
+            ( map {( chr(hex $_) => 'а')}  qw/ 0622 FE81 FE82 / ), # ﺁ
+            ( map {( chr(hex $_) => 'а')}  qw/ 0623 FE83 FE84 / ), # ﺃ
+            ( map {( chr(hex $_) => 'у')}  qw/ 0624 / ), # ؤ
+            ( map {( chr(hex $_) => 'а')}  qw/ 0625 / ), # إ
+            ( map {( chr(hex $_) => 'й')}  qw/ 0626 / ), # ئ
+            ( map {( chr(hex $_) => 'а')}  qw/ 0627 / ), # ا
             ( map {( chr(hex $_) => 'б')}  qw/ 0628 FE8F FE90 FE92 FE91 / ), # ﺏ
-            ( map {( chr(hex $_) => 'т')}  qw/ 062A FE95 FE96 FE98 FE97  062A FE95 FE96 FE98 FE97 / ), # ﺕ ﺙ
+            ( map {( chr(hex $_) => 'ат')} qw/ 0629 / ), # ة
+            ( map {( chr(hex $_) => 'т')}  qw/ 062A FE95 FE96 FE98 FE97 / ), # ﺕ 
+            ( map {( chr(hex $_) => 'т')}  qw/ 062B FE99 FE9A FE9C FE9B / ), # ﺙ
             ( map {( chr(hex $_) => 'дж')} qw/ 062C FE9D FE9E FEA0 FE9F / ), # ﺝ
-            ( map {( chr(hex $_) => 'х')}  qw/ 062D FEA1 FEA2 FEA4 FEA3  062E FEA5 FEA6 FEA8 FEA7 / ), # ﺡ ﺥ
-            ( map {( chr(hex $_) => 'д')}  qw/ 062F FEA9 FEAA  0630 FEAB FEAC / ), # ﺩ ﺫ
+            ( map {( chr(hex $_) => 'х')}  qw/ 062D FEA1 FEA2 FEA4 FEA3 / ), # ﺡ
+            ( map {( chr(hex $_) => 'х')}  qw/ 062E FEA5 FEA6 FEA8 FEA7 / ), # ﺥ
+            ( map {( chr(hex $_) => 'д')}  qw/ 062F FEA9 FEAA / ), # ﺩ
+            ( map {( chr(hex $_) => 'д')}  qw/ 0630 FEAB FEAC / ), # ﺫ
             ( map {( chr(hex $_) => 'р')}  qw/ 0631 FEAD FEAE / ), # ﺭ
             ( map {( chr(hex $_) => 'з')}  qw/ 0632 FEAF FEB0 / ), # ﺯ
             ( map {( chr(hex $_) => 'с')}  qw/ 0633 FEB1 FEB2 FEB4 FEB3 / ), # ﺱ
@@ -53,21 +62,19 @@ our %DATA = (
             ( map {( chr(hex $_) => '')}   qw/ 0639 FEC9 FECA FECC FECB / ), # ﻉ
             ( map {( chr(hex $_) => 'г')}  qw/ 063A FECD FECE FED0 FECF / ), # ﻍ
             ( map {( chr(hex $_) => 'ф')}  qw/ 0641 FED1 FED2 FED4 FED3 / ), # ﻑ
-            ( map {( chr(hex $_) => 'к')}  qw/ 0642 FED5 FED6 FED8 FED7  0643 FED9 FEDA FEDC FEDB / ), # ﻕ ﻙ
+            ( map {( chr(hex $_) => 'к')}  qw/ 0642 FED5 FED6 FED8 FED7 / ), # ﻕ 
+            ( map {( chr(hex $_) => 'к')}  qw/ 0643 FED9 FEDA FEDC FEDB / ), # ﻙ
             ( map {( chr(hex $_) => 'л')}  qw/ 0644 FEDD FEDE FEE0 FEDF / ), # ﻝ
             ( map {( chr(hex $_) => 'м')}  qw/ 0645 FEE1 FEE2 FEE4 FEE3 / ), # ﻡ
             ( map {( chr(hex $_) => 'н')}  qw/ 0646 FEE5 FEE6 FEE8 FEE7 / ), # ﻥ
             ( map {( chr(hex $_) => 'х')}  qw/ 0647 FEE9 FEEA FEEC FEEB / ), # ﻩ
             ( map {( chr(hex $_) => 'у')}  qw/ 0648 FEED FEEE / ), # ﻭ
+            ( map {( chr(hex $_) => 'а')}  qw/ 0649 FEEF FEF0 / ), # ى
             ( map {( chr(hex $_) => 'и')}  qw/ 064A FEF1 FEF2 FEF4 FEF3 / ), # ﻱ
         
-            'ة' => 'ат',
             'ﻻ' => 'ла',
             'ال' => 'аль-',
 
-            'َ' => 'а',
-            'ُ' => 'у',
-            'ِ' => 'и',
             'َا' => 'а',
             'ٰ' => 'а',
             'َى' => 'а',
@@ -78,19 +85,27 @@ our %DATA = (
             'َي' => 'ай',
             'َوّ' => 'ув',
             'ِيّ' => 'ий',
-            'ـً' => 'ан',
-            'ًى' => 'ан',
-            'ـٌ' => 'ун',
-            'ـٍ' => 'ин',
+
+            "\x{064B}" => 'ан',
+            "\x{064B}\x{0649}" => 'ан',
+            "\x{064C}" => 'ун',
+            "\x{064D}" => 'ин',
+            
             'ْ' => '',
             'ّ' => '',
             'ٱ' => '',
             'ؐ' => '',
+
+            # punctuation
+            "\x{060c}" => ',',
+            "\x{061f}" => '?',
+            ( map {( chr(hex $_) => '')} qw/ 0640 / ),
+            # digits
+            ( map {( chr(0x660+$_) => $_ )} ( 0 .. 9 ) ),
         },
         preprocess => sub { my $t = NFC shift(); $t =~ s/\x{200E}//gxms; return $t },
     },
 );
-
 
 sub init {
     my (undef, %callback) = @_;
