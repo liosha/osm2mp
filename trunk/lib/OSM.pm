a613db451bf214c278d8e48dea95c6270c1328b9
xliosha@gmail.com <xliosha@gmail.com@006ae798-83f3-11dd-8a74-1916a93a1be7>
2013-01-11 07:33:32 +0000

    started osm-data storage refactioring

--- trunk/lib/OSM.pm
+++ trunk/lib/OSM.pm
@@ -11,6 +11,7 @@ use warnings;
 use autodie;
 
 use Carp;
+use List::Util qw/ sum /;
 use List::MoreUtils qw/ all notall first_index /;
 
 use Geo::Openstreetmap::Parser;
@@ -29,18 +30,11 @@ Options:
 sub new {
     my ($class, %opt) = @_;
 
-    my $self = {
-        nodes => {},
-        chains => {},
-        tags => { node => {}, way => {}, relation => {} },
-        relations => {},
-    };
-
+    my $self = { data => OSM::Hash->new() };
     bless $self, $class;
 
     if ( $opt{fh} ) {
         $self->load($opt{fh}, %opt);
-        $self->merge_multipolygons();
     }
 
     return $self;
@@ -52,7 +46,9 @@ sub new {
     $osm->load(*STDIN, %opt);
 
 Load osm data from file handler
+
 Options:
+
     skip_tags => [ qw/ source created_by ... / ]
     handlers => { bound => sub{...} }
 
@@ -61,67 +57,34 @@ Options:
 sub load {
     my ($self, $fh, %opt) = @_;
 
-    my ($nodes, $chains, $relations) = @$self{ qw/nodes chains relations/ };
-    my ($nodetag, $waytag, $reltag) = @{$self->{tags}}{ qw/node way relation/ };
-
     my %skip_tag = map {( $_ => 1 )} @{ $opt{skip_tags} || [] };
 
     my %parse_handler = (
         node => sub {
                 my $obj = shift;
-
-                my $attr = $obj->{attr}; 
-                my $id = $attr->{id};
-                $nodes->{$id} = "$attr->{lat},$attr->{lon}";
-
-                if ( my $tags = $obj->{tag} ) {
-                    return if all { $skip_tag{$_} } keys %$tags;
-                    $nodetag->{$id} = $tags;
-                }
-                return;
+                delete $obj->{tag}  if $obj->{tag} && all { $skip_tag{$_} } keys %{$obj->{tag}};
+                return $self->{data}->add_node($obj);
             },
         way => sub {
                 my $obj = shift;
-                my $id = $obj->{attr}->{id};
-
-                my $prev;
-                my @chain =
-                    grep { my $is_dupe = $_ ~~ $prev; $prev = $_; !$is_dupe }
-                    grep { exists $nodes->{$_} }
-                    @{ $obj->{nd} || [] };
-
-                return if @chain < 2;
-
-                $chains->{$id} = \@chain;
-
-                if ( my $tags = $obj->{tag} ) {
-                    return if all { $skip_tag{$_} } keys %$tags;
-                    $waytag->{$id} = $tags;
-                }
-                return;
+                delete $obj->{tag}  if $obj->{tag} && all { $skip_tag{$_} } keys %{$obj->{tag}};
+                return $self->{data}->add_way($obj);
             },
         relation => sub {
                 my $obj = shift;
+                my $type = $obj->{tag}->{type}  or return;
 
-                my $tags = $obj->{tag};
-                return if !$tags;
-
-                my @members =
-                    grep {
-                            my ($id, $t) = @$_{'ref', 'type'};
-                            $t ~~ 'node'     ? exists $nodes->{$id} : 
-                            $t ~~ 'way'      ? exists $chains->{$id} :
-                            $t ~~ 'relation' ? exists $reltag->{$id} :
-                            0;
-                        }
-                    @{ $obj->{member} || [] };
-                return if !@members;
-
-                if ( my $type = $tags->{type} ) {
-                    my $id = $obj->{attr}->{id};
-                    $reltag->{$id} = $tags;
-                    $relations->{$type}->{$id} = \@members;
+                if ( $type ~~ [ qw/ multipolygon boundary / ] ) {
+                    my ($mpoly, $oldstyle_id) = $self->_merge_multipolygon($obj);
+                    if ( $mpoly ) {
+                        my $id = "r$obj->{attr}->{id}";
+                        $self->{data}->add_polygon($id, $mpoly, $obj->{tag});
+                        $self->{data}->add_polygon($oldstyle_id, $mpoly)  if $oldstyle_id;
+                    }
+                    return;
                 }
+
+                $self->{data}->add_relation($obj);                
                 return;
             },
         %{ $opt{handlers} || {} },
@@ -134,54 +97,31 @@ sub load {
 }
 
 
-=method merge_multipolygons
-
-Process multipolygons
-
-=cut
-
-sub merge_multipolygons {
-    my ($self) = @_;
-
-    my $waytag = $self->{tags}->{way};
-    my $reltag = $self->{tags}->{relation};
-
-    for my $type ( qw/ multipolygon boundary / ) {
-        while ( my ($id, $members) = each %{ $self->{relations}->{$type} } ) {
-            my @outers = map {$_->{ref}} grep { $_->{type} ~~ 'way' && $_->{role} ~~ [ q{}, 'outer', 'exclave' ] } @$members;
-            next if !@outers;
-
-            my @inners = map {$_->{ref}} grep { $_->{type} ~~ 'way' && $_->{role} ~~ [ 'inner', 'enclave' ] } @$members;
-
-            my $mpoly = _merge_multipolygon($self, \@outers, \@inners);
-            next if !$mpoly;
-
-            my $rid = "r$id";
-            $self->{mpoly}->{$rid} = $mpoly;
-            $self->{mpoly}->{$outers[0]} = $mpoly   if @outers == 1 && @inners;
-            $waytag->{$rid} = $reltag->{$id};
-        }
-    }
-    return;
-}
-
 
 sub _merge_multipolygon {
-    my ($self, $outers, $inners) = @_;
+    my ($self, $obj) = @_;
 
-    my $chains = $self->{chains};
+    my @members = grep { $_->{type} ~~ 'way' } @{ $obj->{member} };
+
+    my @outer_ids = map { $_->{ref} }  grep { $_->{role} ~~ [ q{}, 'outer', 'exclave' ] }  @members;
+    return if !@outer_ids;
+
+    my @inner_ids = map { $_->{ref} }  grep { $_->{role} ~~ [ 'inner', 'enclave' ] }  @members;
+
+    my ($oldstyle_id) = (@outer_ids == 1 && @inner_ids) ? @outer_ids : ();
 
     my @result;
-    for my $list_ref ( [@$outers], [@$inners] ) {
+    for my $list_ref ( \@outer_ids, \@inner_ids ) {
         my @rings;
 
         # skip incomplete objects
-        return if notall { exists $chains->{$_} } @$list_ref;
+        return if notall { $self->{data}->is_way_exists($_) } @$list_ref;
+        my %chain = map {( $_ => $self->{data}->get_way_chain($_) )} @$list_ref;
 
         LIST:
         while ( @$list_ref ) {
             my $id = shift @$list_ref;
-            my @contour = @{$chains->{$id}};
+            my @contour = @{ $chain{$id} };
 
             CONTOUR: {
                 if ( $contour[0] eq $contour[-1] ) {
@@ -189,22 +129,22 @@ sub _merge_multipolygon {
                     next LIST;
                 }
 
-                my $add = first_index { $contour[-1] eq $chains->{$_}->[0] } @$list_ref;
+                my $add = first_index { $contour[-1] eq $chain{$_}->[0] } @$list_ref;
                 if ( $add > -1 ) {
                     my $add_id = $list_ref->[$add];
                     $id .= ":$add_id";
                     pop @contour;
-                    push @contour, @{$chains->{$add_id}};
+                    push @contour, @{ $chain{$add_id} };
                     splice @$list_ref, $add, 1;
                     redo CONTOUR;
                 }
 
-                $add = first_index { $contour[-1] eq $chains->{$_}->[-1] } @$list_ref;
+                $add = first_index { $contour[-1] eq $chain{$_}->[-1] } @$list_ref;
                 if ( $add > -1 ) {
                     my $add_id = $list_ref->[$add];
                     $id .= ":r$add_id";
                     pop @contour;
-                    push @contour, reverse @{$chains->{$add_id}};
+                    push @contour, reverse @{ $chain{$add_id} };
                     splice @$list_ref, $add, 1;
                     redo CONTOUR;
                 }
@@ -217,9 +157,113 @@ sub _merge_multipolygon {
         push @result, \@rings;
     }
 
-    return \@result;
+    return \@result, $oldstyle_id;
 }
 
+
+
+# factoring out storing engine
+package OSM::Hash;
+
+sub new {
+    my ($class, %opt) = @_;
+
+    my $self = {
+        nodes => {},
+        chains => {},
+        tags => { node => {}, way => {}, relation => {} },
+        relations => {},
+    };
+
+    return bless $self, $class;
+}
+
+
+sub add_node {
+    my ($self, $obj) = @_;
+
+    my $attr = $obj->{attr};
+    my $id = $attr->{id};
+    $self->{nodes}->{$id} = "$attr->{lat},$attr->{lon}";
+    $self->{tags}->{node}->{$id} = $obj->{tag}  if $obj->{tag};
+
+    return;
+}
+
+
+sub is_node_exists {
+    my ($self, $id) = @_;
+
+    return exists $self->{nodes}->{$id};
+}
+
+
+sub add_way {
+    my ($self, $obj) = @_;
+
+    my $id = $obj->{attr}->{id};
+    
+    my $prev;
+    my @chain =
+        grep { my $is_dupe = $_ ~~ $prev; $prev = $_; !$is_dupe }
+        grep { exists $self->{nodes}->{$_} }
+        @{ $obj->{nd} || [] };
+    
+    return if @chain < 2;
+
+    $self->{chains}->{$id} = \@chain;
+    $self->{tags}->{way}->{$id} = $obj->{tag}  if $obj->{tag};
+
+    return;
+}
+
+
+sub is_way_exists {
+    my ($self, $id) = @_;
+
+    return exists $self->{chains}->{$id};
+}
+
+
+sub get_way_chain {
+    my ($self, $id) = @_;
+
+    return $self->{chains}->{$id};
+}
+
+
+sub add_polygon {
+    my ($self, $id, $mpoly, $tags) = @_;
+    
+    $self->{mpoly}->{$id} = $mpoly;
+    $self->{tags}->{way}->{$id} = $tags  if $tags;
+    return;
+}
+
+
+sub add_relation {
+    my ($self, $obj) = @_;
+
+    my $tags = $obj->{tag};
+    return if !$tags;
+
+    my @members =
+        grep {
+            my ($id, $t) = @$_{'ref', 'type'};
+            $t ~~ 'node'     ? exists $self->{nodes}->{$id} :
+            $t ~~ 'way'      ? exists $self->{chains}->{$id} :
+            $t ~~ 'relation' ? exists $self->{tags}->{relation}->{$id} :
+            0;
+        }
+        @{ $obj->{member} || [] };
+    return if !@members;
+
+    my $id = $obj->{attr}->{id};
+    $self->{tags}->{relation}->{$id} = $tags;
+    $self->{relations}->{$tags->{type}}->{$id} = \@members;
+                
+    return;
+}
 
 
 1;
