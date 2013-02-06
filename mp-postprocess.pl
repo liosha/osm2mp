77acfd636435915d8b441ac8d848524c52eab1a6
xliosha@gmail.com <xliosha@gmail.com@006ae798-83f3-11dd-8a74-1916a93a1be7>
2013-02-06 11:36:54 +0000

    postprocess: refactoring

--- mp-postprocess.pl
+++ mp-postprocess.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 
+use 5.010;
 use strict;
+use warnings;
 use utf8;
 
 my $file = shift @ARGV;
@@ -12,28 +14,6 @@ open my $in,  '<:encoding(cp1251)', "$file.old";
 open my $out, '>:encoding(cp1251)', $file;
 
 
-my @short = (
-    [ 'ул(|ица)'                =>  'ул.'    ],
-    [ 'пер(|еул|еулок)'         =>  'пер.'   ],
-    [ 'пр(\-к?т|осп|оспект)'    =>  'пр-т'   ],
-    [ 'пр(\-з?д|оезд)'          =>  'пр-д'   ],
-    [ 'п()'                     =>  'п.'     ],
-    [ 'пр()'                    =>  'пр.'    ],
-    [ 'пл(|ощадь)'              =>  'пл.'    ],
-    [ 'ш(|оссе)'                =>  'ш.'     ],
-    [ 'туп(|ик)'                =>  'туп.'   ],
-    [ 'б(ул|ульв|-р|ульвар)'    =>  'б-р'    ],
-    [ 'наб(|ережная)'           =>  'наб.'   ],
-    [ 'ал(|лея)'                =>  'ал.'    ],
-    [ 'мост()'                  =>  'мост'   ],
-    [ 'тракт()'                 =>  'тракт'  ],
-    [ 'просек()'                =>  'просек' ],
-    [ 'линия()'                 =>  'линия'  ],
-    [ 'кв(|арт|артал)'          =>  'кв.'  ],
-    [ 'м(к?рн?|икрорайон)'      =>  'мкр'  ],
-);
-
-
 my $object;
 
 LINE:
@@ -42,62 +22,115 @@ while ( my $line = readline $in ) {
     if ( $line =~ /^\[(.*)\]/ ) {
         $object = $1;
     }
-    
+
+    my ($tag, $val) = $line =~ / ^ \s* (\w+) \s* = \s* (.*\S) \s* $ /xms;
+
+    if ( $tag ~~ [ qw/ Label StreetDesc CityName RegionName / ] ) {
+        $val = _clear_bad_symbols($val);
+    }
+
     #   region name
-    if ( $line =~ /^(DefaultRegionCountry|RegionName)/i ) {
-        $line =~ s/область/обл./;
-        $line =~ s/район/р-н/;
-        $line =~ s/Автономный округ - Югра автономный округ/АО (Югра)/;
-        $line =~ s/Кабардино-Балкарская республика/Кабардино-Балкария/;
-        $line =~ s/Карачаево-Черкесская республика/Карачаево-Черкесия/;
-        $line =~ s/автономный округ/АО/i;
-        $line =~ s/автономная обл./АО/i;
-        $line =~ s/ город//i;
-        $line =~ s/([ие]я|[^я]) республика/$1/i;
-        $line =~ s/ - Алания//;
+    if ( $tag ~~ [ qw/DefaultRegionCountry RegionName/ ] ) {
+        $val = _clear_region($val);
     }
-
-    if ( $line =~ /^(Label|StreetDesc|CityName|RegionName)=/i ) {
-        $line =~ s/городской округ/ГО/ig;
-        $line =~ s/[«»"]//g;
-        $line =~ s/&#xD;//g;
-    }
-
 
     #   street names
-    my $tag;
-    if ( $object eq 'POLYLINE' && ( ($tag) = $line =~ /^(Label\d?)=/i ) 
-            or ( ($tag) = $line =~ /^(StreetDesc)=/i ) ) {
-        
-        $line = join q{ }, map { ucfirst } grep { $_ } split / /, $line;
-        
-        SUFF:
-        for my $short ( @short ) {
-            my ( $s, $r ) = @$short;
-            next SUFF unless
-                my ( undef, $prefix, undef, undef, undef, $postfix )
-                    = $line =~ /=((.*\S)?\s+)?$s((\s+|\s*\.\s*)(.*))?$/i;
-            
-            next SUFF unless $prefix || $postfix;
-            
-            $line = "$prefix $postfix";
-
-            $line =~ s/(\d+-?.?[йяе])(\s+(.*))/$2 $1/;
-            $line =~ s/(\d+)-?.?([йяе])(\s.*)?$/$1-$2/;
-
-            $line =~ s/\s\s+/ /;
-            $line =~ s/^ //;
-            $line =~ s/ $//;
-
-            $line = "$tag=" . $line . " " . $r . "\n";
-            last SUFF;
-        }
+    if ( ($object eq 'POLYLINE' && $tag ~~ 'Label') || $tag ~~ 'StreetDesc' ) {
+        $val = _clear_street($val);
     }
 
-    print $out $line;
+    $line = "$tag=$val\n"  if $tag;
+    print {$out} $line;
 }
 
 close $in;
 close $out;
 
 unlink "$file.old";
+
+exit;
+
+
+BEGIN {
+my @short_names = (
+    [ 'ул(?:|ица)'              =>  'ул.'    ],
+    [ 'пер(?:|еул|еулок)'       =>  'пер.'   ],
+    [ 'пр(?:\-к?т|осп|оспект)'  =>  'пр-т'   ],
+    [ 'пр(?:\-з?д|оезд)'        =>  'пр-д'   ],
+    [ 'п'                       =>  'п.'     ],
+    [ 'пр'                      =>  'пр.'    ],
+    [ 'пл(?:|ощадь)'            =>  'пл.'    ],
+    [ 'ш(?:|оссе)'              =>  'ш.'     ],
+    [ 'туп(?:|ик)'              =>  'туп.'   ],
+    [ 'б(?:ул|ульв|-р|ульвар)'  =>  'б-р'    ],
+    [ 'наб(?:|ережная)'         =>  'наб.'   ],
+    [ 'ал(?:|лея)'              =>  'ал.'    ],
+    [ 'мост'                    =>  'мост'   ],
+    [ 'тракт'                   =>  'тракт'  ],
+    [ 'просек'                  =>  'просек' ],
+    [ 'линия'                   =>  'линия'  ],
+    [ 'кв(?:|арт|артал)'        =>  'кв.'  ],
+    [ 'м(?:к?рн?|икрорайон)'    =>  'мкр'  ],
+);
+
+sub _clear_street {
+    my ($in) = @_;
+
+    my $line = join q{ }, map { ucfirst } grep { $_ } split / /, $in;
+
+    SUFF:
+    for my $short ( @short_names ) {
+        my ( $s, $r ) = @$short;
+        next SUFF unless
+            my ( $prefix, $postfix )
+                = $line =~ / ^ (?: (.*\S)? \s+ )? $s (?: (?: \s+ | \s*\.\s* ) (.*) )? $ /ix;
+
+        next SUFF unless $prefix || $postfix;
+
+        $line = join q{ }, grep {$_} ($prefix, $postfix);
+
+        $line =~ s/(\d+-?.?[йяе])(\s+(.*))/$2 $1/;
+        $line =~ s/(\d+)-?.?([йяе])(\s.*)?$/$1-$2/;
+
+        $line =~ s/\s\s+/ /;
+        $line =~ s/^ //;
+        $line =~ s/ $//;
+        return "$line $r";
+    }
+
+    return $in;
+}
+}
+
+
+
+
+
+sub _clear_region {
+    my ($line) = @_;
+    
+    $line =~ s/область/обл./;
+    $line =~ s/район/р-н/;
+    $line =~ s/Автономный округ - Югра автономный округ/АО (Югра)/;
+    $line =~ s/Кабардино-Балкарская республика/Кабардино-Балкария/;
+    $line =~ s/Карачаево-Черкесская республика/Карачаево-Черкесия/;
+    $line =~ s/автономный округ/АО/i;
+    $line =~ s/автономная обл./АО/i;
+    $line =~ s/городской округ/ГО/ig;
+    $line =~ s/ город//i;
+    $line =~ s/([ие]я|[^я]) республика/$1/i;
+    $line =~ s/ - Алания//;
+
+    return $line;
+}
+
+
+sub _clear_bad_symbols {
+    my ($line) = @_;
+
+    $line =~ s/[«»"]//g;
+    $line =~ s/&#xD;//g;
+
+    return $line;
+}
+
