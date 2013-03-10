a1f033cd5d4342ae9bfd85a203bf116e5d44796f
xliosha@gmail.com <xliosha@gmail.com@006ae798-83f3-11dd-8a74-1916a93a1be7>
2013-03-10 07:50:32 +0000

    lt: bg-ru transcription

--- lib/LangTransform/Subst.pm
+++ lib/LangTransform/Subst.pm
@@ -57,6 +57,23 @@ our %DATA = (
         },
         same_upcase => 1,
     },
+    # http://ru.wikipedia.org/wiki/%C1%EE%EB%E3%E0%F0%F1%EA%E8%E9_%E0%EB%F4%E0%E2%E8%F2#.D0.9F.D1.80.D0.B0.D0.BA.D1.82.D0.B8.D1.87.D0.B5.D1.81.D0.BA.D0.B0.D1.8F_.D1.82.D1.80.D0.B0.D0.BD.D1.81.D0.BA.D1.80.D0.B8.D0.BF.D1.86.D0.B8.D1.8F_.D0.BD.D0.B0_.D1.80.D1.83.D1.81.D1.81.D0.BA.D0.B8.D0.B9_.D1.8F.D0.B7.D1.8B.D0.BA
+    bg_ru => {
+        from  => 'bg',
+        to    => 'ru',
+        table => {
+            'щ' => 'шч',
+            'ъ' => 'ы',
+            '\bъ' => 'и',
+            '(?<=[жшц])ъ' => 'е',
+            '(?<=б)ъ(?=лгар)' => 'о',
+            '(?<=п)ъ(?=рв)' => 'е',
+            'ьо' => 'ё',
+            'йо' => 'ё',
+            '\bйо' => 'йо',
+        },
+        same_upcase => 1,
+    },
     ar_ru => {
         # http://ru.wikipedia.org/wiki/Арабско-русская_практическая_транскрипция
         # http://en.wikipedia.org/wiki/Arabic_(Unicode_block)
